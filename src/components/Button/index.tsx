2f353e3bfdfbfe0a7644d54aeb0b14d766f82a7f
matheus.araujo <matheus.araujo@trinusco.com.br>
2024-03-07 04:28:29 +0000

    learn about style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default class Button extends React.Component {
   render(): React.ReactNode {
+    const isActive = false
+    const styles = {
+      backgroundColor: isActive ? "green" : "red"
+    }
     return (
-      <button>
+      <button style={styles}>
         Botão
       </button>
     )
