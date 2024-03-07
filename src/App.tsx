7ce31aaf0652e4dddd6650776a4272ab554da500
matheus.araujo <matheus.araujo@trinusco.com.br>
2024-03-07 03:59:19 +0000

    Criando a lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Form from './components/form';
-
+import List from './components/List';
 function App() {
   return (
     <div className="App">
       <Form></Form>
+      <List></List>
     </div>
   );
 }
